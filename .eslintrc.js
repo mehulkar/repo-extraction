15d347568a79e45dc2f679b235e47e7036e26980
Mehul Kar <mehul.kar@gmail.com>
2020-02-11 16:33:10 -0800

    prettier on eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "prettier"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "plugins": [
-        "prettier"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  rules: {}
 };
